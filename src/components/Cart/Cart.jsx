8d66efaa401023c1243d931a53b41fc68fbff234
Royal Finch <Royal2791@gmail.com>
2024-03-12 17:39:11 -0400

    remove quantity from Item component
    * Plan to move quantity to Cart/Item page

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,3 +3,32 @@ const Cart = () => {
 };
 
 export default Cart;
+
+// const increment = () => {
+//   if (quantity < limit) setQuantity((prev) => parseInt(prev) + 1);
+// };
+
+// const decrement = () => {
+//   if (quantity > 0) setQuantity((prev) => parseInt(prev) - 1);
+// };
+
+// const handleChange = (event) => {
+//   const { value } = event.target;
+//   setQuantity(value > limit ? limit : value < 0 ? 0 : value);
+// };
+
+// <form className={styles.quantity}>
+//         <button type="button" onClick={decrement}>
+//           -
+//         </button>
+//         <input
+//           type="number"
+//           onChange={handleChange}
+//           value={quantity}
+//           min="0"
+//           max={limit}
+//         />
+//         <button type="button" onClick={increment}>
+//           +
+//         </button>
+//       </form>
--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -5,39 +5,12 @@ const Item = ({ item }) => {
   const [quantity, setQuantity] = useState(0);
   const { imgURL, name, limit } = item;
 
-  const handleChange = (event) => {
-    const { value } = event.target;
-    setQuantity(value > limit ? limit : value < 0 ? 0 : value);
-  };
-
-  const increment = () => {
-    if (quantity < limit) setQuantity((prev) => parseInt(prev) + 1);
-  };
-
-  const decrement = () => {
-    if (quantity > 0) setQuantity((prev) => parseInt(prev) - 1);
-  };
   return (
     <div className={styles.container}>
       <div className={styles.imgContainer}>
         <img className={styles.itemImg} src={imgURL} alt={name} />
       </div>
       <h4 className={styles.text}>{name}</h4>
-      <form className={styles.quantity}>
-        <button type="button" onClick={decrement}>
-          -
-        </button>
-        <input
-          type="number"
-          onChange={handleChange}
-          value={quantity}
-          min="0"
-          max={limit}
-        />
-        <button type="button" onClick={increment}>
-          +
-        </button>
-      </form>
     </div>
   );
 };
